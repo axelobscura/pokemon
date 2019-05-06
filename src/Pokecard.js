3614868e56da6c5bb8063e01839744942e5fa30b
Axel <axosar@gmail.com>
2019-05-06 12:16:46 -0500

    Last steps
    Completing the poem on react game…

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,16 +1,18 @@
 import React, {Component} from 'react';
 import './Pokecard.css';
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+
+let pathToTree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
 
 class Pokecard extends Component {
   render(){
-    let imgSrc = `${POKE_API}${this.props.id}.png`;
+    let imgSrc = `${POKE_API}${pathToTree(this.props.id)}.png`;
     return(
       <div className="Pokecard">
-        <h1>{this.props.name}</h1>
-        <img src={imgSrc} alt="W"/>
-        <div>Type: {this.props.type}</div>
-        <div>EXP: {this.props.exp}</div>
+        <h1 className="Pokecard-title">{this.props.name}</h1>
+        <div className="Pokecard-image"><img src={imgSrc} alt="W"/></div>
+        <div className="Pkecard-data">Type: {this.props.type}</div>
+        <div className="Pkecard-data">EXP: {this.props.exp}</div>
       </div>
     )
   }
--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
 import Pokecard from './Pokecard';
+import './Pokedex.css';
 
 class Pokedex extends Component {
-  static defaultProps = {
-    pokemon:[
-      {id: 4, name: 'Charmander', type: 'fire', base_experience: 62},
-      {id: 7, name: 'Squirtle', type: 'water', base_experience: 63},
-      {id: 11, name: 'Metapod', type: 'bug', base_experience: 72},
-      {id: 12, name: 'Butterfree', type: 'flying', base_experience: 178},
-      {id: 25, name: 'Pikachu', type: 'electric', base_experience: 112},
-      {id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95},
-      {id: 94, name: 'Gengar', type: 'poison', base_experience: 225},
-      {id: 133, name: 'Eevee', type: 'normal', base_experience: 65}
-    ]
-  };
   render(){
+    let title;
+    if(this.props.isWinner){
+      title = <h1 className="pokedex-winner">Winning Hand</h1>
+    } else {
+      title = <h1 className="pokedex-loser">Losing Hand</h1>
+    }
     return(
       <div className="pokeDex">
-        <h1>Pokedex APP!</h1>
-        {this.props.pokemon.map((p) => (
-          <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
-        ))}
+        {title}
+        <h4>Total experience: {this.props.exp}</h4>
+        <div className="Pokedex-cards">
+          {this.props.pokemon.map((p) => (
+            <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
+          ))}
+        </div>
       </div>
     );
   }
