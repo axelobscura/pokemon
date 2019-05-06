d1fc1956f9a46074f4c8098793bddf5f34158941
Axel <axosar@gmail.com>
2019-05-06 16:36:48 -0500

    Events in React
    Handling events in react js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 //import Pokedex from './Pokedex';
 import Pokegame from './Pokegame';
+import Brokenclick from './Brokenclick';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Pokegame />
+      <Brokenclick />
     </div>
   );
 }
